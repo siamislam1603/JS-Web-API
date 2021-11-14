80df85e111cbf0f64dd2954aa6db7c3b99801665
Your Name <you@example.com>
2021-11-14 20:29:29 +0600

    Modified the indentation of JSON file

--- json.js
+++ json.js
@@ -1,12 +1,12 @@
 console.log("Javascript object notation (JSON)");
-    const student = {
-      id: 12,
-      name: "Jamal",
-      favSongs: ["Believer", "Unstoppable", "Hall of Fame"],
-      friends: ["Rashid", "Hasan", "Jalal"],
-    };
-    const studentJSON = JSON.stringify(student); //convert it into json
-    console.log(studentJSON);
-    // {"id":12,"name":"Jamal","favSongs":["Believer","Unstoppable","Hall of Fame"] ,"friends":["Rashid","Hasan","Jalal"]}
-    const JSONToObj = JSON.parse(studentJSON);
-    console.log(JSONToObj);
+const student = {
+    id: 12,
+    name: "Jamal",
+    favSongs: ["Believer", "Unstoppable", "Hall of Fame"],
+    friends: ["Rashid", "Hasan", "Jalal"],
+};
+const studentJSON = JSON.stringify(student); //convert it into json
+console.log(studentJSON);
+// {"id":12,"name":"Jamal","favSongs":["Believer","Unstoppable","Hall of Fame"] ,"friends":["Rashid","Hasan","Jalal"]}
+const JSONToObj = JSON.parse(studentJSON);
+console.log(JSONToObj);
